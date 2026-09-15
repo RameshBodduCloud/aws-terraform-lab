27a5fb1344e4feb69f0ef099a9c73e56fd11a48f
RameshBodduCloud <ramesh29699@gmail.com>
2026-09-15 16:47:37 +0530

    Terraform configs Day-01, ignore state/cache files

---
+++ Day-01(Tf configs -vpc creation)/main.tf
@@ -0,0 +1,17 @@
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name        = "MyVPC"
+  }
+}
+
+# Create S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "ramesh-terraform-lab-bucket"   # must be globally unique
+
+  tags = {
+    Name        = "MyBucket"
+  }
+}
