c1bbf3a7a8f953fd2c861b0598753d329f2e1932
RameshBodduCloud <ramesh29699@gmail.com>
2026-09-15 21:47:45 +0530

    Terraform configs Day-01, ignore state/cache files

--- Day-01(Tf configs -vpc creation)/main.tf
+++ Day-01(Tf configs -vpc creation)/main.tf
@@ -3,15 +3,15 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name        = "MyVPC"
+    Name = "MyVPC"
   }
 }
 
 # Create S3 Bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "ramesh-terraform-lab-bucket"   # must be globally unique
+  bucket = "ramesh-terraform-lab-bucket"  # must be globally unique
 
   tags = {
-    Name        = "MyBucket"
+    Name = "MyBucket"
   }
 }
