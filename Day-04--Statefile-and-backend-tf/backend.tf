a8771092298af78abe1ac1db702c0f1dd4214b6f
RameshBodduCloud <ramesh29699@gmail.com>
2026-09-19 01:35:19 +0530

    stet migrated to s3 bucket using backend

---
+++ Day-04--Statefile-and-backend-tf/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "ramesh-terraform-state-bucket1"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "Ramesh-state-lock"
+    #encrypt        = true
+  }
+}
